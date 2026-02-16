96fd3e379ff6768d66bbbe1630ad4c3b18c798e5
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2026-02-15 21:56:59 -0600

    Update handler and file paths in concon.js

--- public/uv/concon.js
+++ public/uv/concon.js
@@ -2,9 +2,9 @@ self.__uv$config = {
 	prefix: '/b/u/hi/',
 	encodeUrl: Ultraviolet.codec.base64.encode,
 	decodeUrl: Ultraviolet.codec.base64.decode,
-	handler: '/b/u/hanhan.js',
-	client: '/b/u/clicli.js',
-	bundle: '/b/u/bunbun.js',
-	config: '/b/u/concon.js',
-	sw: '/b/u/serser.js'
+	handler: '/uv/hanhan.js',
+	client: '/uv/clicli.js',
+	bundle: '/uv/bunbun.js',
+	config: '/uv/concon.js',
+	sw: '/uv/serser.js'
   };
