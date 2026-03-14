560cab94f18062c1d2a8ce72fbf258d75e6e538c
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2026-03-13 23:06:50 -0500

    Update uv.config.js

--- public/uv.config.js
+++ public/uv.config.js
@@ -1,11 +1,12 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/uv/service/',
+    prefix: "/service/",
+    bare: "/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv.handler.js',
-    client: '/uv.client.js',
-    bundle: '/uv.bundle.js',
-    config: '/uv.config.js',
-    sw: '/uv.sw.js',
+    handler: "/uv.handler.js",
+    client: "/uv.client.js",
+    bundle: "/uv.bundle.js",
+    config: "/uv.config.js",
+    sw: "/uv.sw.js",
 };
